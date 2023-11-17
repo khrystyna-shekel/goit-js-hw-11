80b99c411cad9d34ced4abd50145d522e4eec4be
khrystyna-shekel <christina.shekel@gmail.com>
2023-11-17 22:16:59 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+// import Notiflix from 'notiflix';
+
+
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Block } from 'notiflix/build/notiflix-block-aio';
