9a43690d1f5ef6178e520bd19a03e261aa72d9f8
khrystyna-shekel <christina.shekel@gmail.com>
2023-11-20 00:13:47 +0200

    gradient added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,118 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { Report } from 'notiflix/build/notiflix-report-aio';
-// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-// import { Loading } from 'notiflix/build/notiflix-loading-aio';
-// import { Block } from 'notiflix/build/notiflix-block-aio';
+import './styles.css';
+import PixabayApi from './pixabay-api';
+import { lightbox } from './lightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryWrapper: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+const { searchForm, galleryWrapper, loadMoreBtn } = refs;
+
+let isShown = 0;
+const pixabayApi = new PixabayApi();
+
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+const options = {
+  rootMargin: '100px',
+  root: null,
+  threshold: 0.3,
+};
+const observer = new IntersectionObserver(onLoadMoreBtnClick, options);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  galleryWrapper.innerHTML = '';
+  pixabayApi.query =
+    e.currentTarget.elements.searchQuery.value.trim().toLowerCase();
+  pixabayApi.resetPage();
+
+  if (pixabayApi.query === '') {
+    Notify.warning('Please, fill the field!');
+    return;
+  }
+
+  isShown = 0;
+  fetchGallery();
+  onGetGallery(hits);
+}
+
+function onLoadMoreBtnClick() {
+  pixabayApi.incrementPage();
+  fetchGallery();
+}
+
+async function fetchGallery() {
+  loadMoreBtn.classList.add('is-hidden');
+
+  const result = await pixabayApi.fetchGallery();
+  const { hits, total } = result;
+  isShown += hits.length;
+
+  if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+
+  onGetGallery(hits);
+  isShown += hits.length;
+
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    loadMoreBtn.classList.remove('is-hidden');
+  }
+
+  if (isShown >= total) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
+
+function onGetGallery(elements) {
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+      </p>
+    </div>
+    </div>`;
+      }
+    )
+    .join('');
+  galleryWrapper.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
---
+++ src/pixabay-api.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '40750466-a9a3b4ee49fa6557c8d09caed';
+
+export default class PixabayApi {
+    constructor(){
+        this.page = 1;
+        this.PER_PAGE = 40;
+        this.searchQuery = '';
+    }
+    
+    async fetchGallery() {
+        const axiosOptions = {
+            method: 'get',
+            url: `${BASE_URL}`,
+            params: {
+                key: `${API_KEY}`,
+                q: `${this.searchQuery}`,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: `${this.page}`,
+                per_page: `${this.PER_PAGE}`,
+            },
+        };
+        try {
+            const response = await axios(axiosOptions);
+
+            const data = response.data;
+
+            this.incrementPage();
+            return data;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    resetEndOfHits() {
+        this.endOfHits = false;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
